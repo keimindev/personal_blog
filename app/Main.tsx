7759847d8cca4a3364ae21cb408094bce20898a2
keimindev <keimindev@gmail.com>
2024-07-16 23:06:25 +0900

    [Feat] change Main page UI

--- app/Main.tsx
+++ app/Main.tsx
@@ -13,20 +13,23 @@ export default function Home({ posts }) {
         </section>
         <section className="w-full">
           <div className="text-4xl">
-            hello, I'm <span className="font-bold">Frontend Developer</span>
+            <p>Hello,</p>
+            <p>
+              I'm <span className="text-5xl font-bold">Frontend Developer</span>
+            </p>
           </div>
-          <div>
+          <div className="py-5">
             A frontend developer who prioritizes creating better user value over simply making a
             product.
           </div>
-          <div className="flex w-full items-center justify-between p-5">
+          <div className="mt-10 flex w-full items-center justify-between px-5">
             {headerNavLinks
               .filter((link) => link.href !== '/')
               .map((link) => (
                 <Link
                   key={link.title}
                   href={link.href}
-                  className="hidden font-medium text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400
+                  className="hidden text-xl font-bold text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400
               sm:block"
                 >
                   {link.title}
