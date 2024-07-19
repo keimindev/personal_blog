f7e347c288a6b40bebeedd2ef936c28db125b8a0
keimindev <keimindev@gmail.com>
2024-07-20 01:43:38 +0900

    [Feat] update Project contents

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,14 +7,26 @@ interface Project {
 
 const projectsData: Project[] = [
   {
+    title: "Ming's devlog",
+    description: `Ming tech blog & portfolio`,
+    imgSrc: '/static/images/project/personal_blog.png',
+    href: '/blog/projects/personal-blog',
+  },
+  {
     title: 'SafeMoney',
-    description: `1:1 Transaction platform`,
+    description: `A 1:1 trading platform for secure transactions.`,
     imgSrc: '/static/images/project/safemoney-1.png',
     href: '/blog/projects/safemoney',
   },
   {
+    title: 'T.O.T',
+    description: `A time tracking platform`,
+    imgSrc: '/static/images/project/tot.png',
+    href: '/blog/projects/tot',
+  },
+  {
     title: 'Netflix OX Quiz',
-    description: `A Netflix content-based OX quiz webpage. It is optimized for mobile and developed using React and Redux. It is a simple quiz game webpage where you can compare your score with others' scores.`,
+    description: `A Netflix content-based OX quiz webpage. `,
     imgSrc: '/static/images/project/netflix_ox.png',
     href: '/blog/projects/netflix-ox-quiz',
   },
