846b88e9860c47cd81a7d3cace3afc4dfd56f7cf
keimindev <keimindev@gmail.com>
2024-08-01 13:13:04 +0900

    [Fix] loading main character

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,15 +1,25 @@
 'use client'
 
 import Link from '@/components/Link'
-import MainCharacter from '@/components/MainCharacter'
 import headerNavLinks from '@/data/headerNavLinks'
+import { Fit, Layout, useRive } from '@rive-app/react-canvas'
 
 export default function Home({ posts }) {
+  const { rive, RiveComponent } = useRive({
+    src: '/min.riv',
+    animations: 'EyeBlink',
+    autoplay: true,
+    stateMachines: 'State Machine 1',
+    layout: new Layout({
+      fit: Fit.Cover,
+    }),
+  })
+
   return (
     <>
       <div className="flex w-full flex-col items-center justify-between md:flex-row">
         <section className="aspect-ratio">
-          <MainCharacter />
+          <RiveComponent onMouseEnter={() => rive && rive.play()} />
         </section>
         <section className="mt-20 w-full text-center md:mt-0 md:text-start">
           <div className="text-4xl">
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -6,12 +6,12 @@ interface Project {
 }
 
 const projectsData: Project[] = [
-  {
-    title: "Ming's devlog",
-    description: `Ming tech blog & portfolio`,
-    imgSrc: '/static/images/project/personal_blog.png',
-    href: '/blog/projects/personal-blog',
-  },
+  // {
+  //   title: "Ming's devlog",
+  //   description: `Ming tech blog & portfolio`,
+  //   imgSrc: '/static/images/project/personal_blog.png',
+  //   href: '/blog/projects/personal-blog',
+  // },
   {
     title: 'SafeMoney',
     description: `A 1:1 trading platform for secure transactions.`,
@@ -24,12 +24,12 @@ const projectsData: Project[] = [
     imgSrc: '/static/images/project/tot.png',
     href: '/blog/projects/tot',
   },
-  {
-    title: 'Netflix OX Quiz',
-    description: `A Netflix content-based OX quiz webpage. `,
-    imgSrc: '/static/images/project/netflix_ox.png',
-    href: '/blog/projects/netflix-ox-quiz',
-  },
+  // {
+  //   title: 'Netflix OX Quiz',
+  //   description: `A Netflix content-based OX quiz webpage. `,
+  //   imgSrc: '/static/images/project/netflix_ox.png',
+  //   href: '/blog/projects/netflix-ox-quiz',
+  // },
 ]
 
 export default projectsData
