3df29cbca2215b8892138b3deabc2cc4c1941313
keimindev <keimindev@gmail.com>
2024-11-04 16:55:47 +0900

    add projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -13,23 +13,29 @@ const projectsData: Project[] = [
   //   href: '/blog/projects/personal-blog',
   // },
   {
+    title: 'T.O.T App ver',
+    description: `A time tracking platform`,
+    imgSrc: '/static/images/project/tot-app.png',
+    href: '/blog/projects/tot',
+  },
+  {
     title: 'SafeMoney',
     description: `A 1:1 trading platform for secure transactions.`,
     imgSrc: '/static/images/project/safemoney-1.png',
     href: '/blog/projects/safemoney',
   },
   {
-    title: 'T.O.T',
+    title: 'T.O.T (NextJS)',
     description: `A time tracking platform`,
     imgSrc: '/static/images/project/tot.png',
     href: '/blog/projects/tot',
   },
-  // {
-  //   title: 'Netflix OX Quiz',
-  //   description: `A Netflix content-based OX quiz webpage. `,
-  //   imgSrc: '/static/images/project/netflix_ox.png',
-  //   href: '/blog/projects/netflix-ox-quiz',
-  // },
+  {
+    title: 'Netflix OX Quiz',
+    description: `A Netflix content-based OX quiz webpage. `,
+    imgSrc: '/static/images/project/netflix_ox.png',
+    href: '/blog/projects/netflix-ox-quiz',
+  },
 ]
 
 export default projectsData
