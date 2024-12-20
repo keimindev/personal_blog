c15eeb2e43d94567c9583976d8f29589c4b62087
keimindev <keimindev@gmail.com>
2024-12-20 14:33:30 +0900

    changed author name

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
   title: "Ming's Dev",
-  author: 'MinGyeong',
+  author: 'Keira M J',
   headerTitle: 'Ming',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
