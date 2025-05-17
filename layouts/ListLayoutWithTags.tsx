2ec5f60e67022fe018704458c1cb5bee2354a7bb
keimindev <keimindev@gmail.com>
2025-05-17 16:27:11 -0400

    fix : modify blog list ui

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -83,31 +83,35 @@ export default function ListLayoutWithTags({
             {title}
           </h1>
         </div>
-        <div className="flex sm:space-x-24">
-          <div className="hidden h-full max-h-screen min-w-[280px] max-w-[280px] flex-wrap overflow-auto rounded bg-gray-50 pt-5 shadow-md dark:bg-gray-900/70 dark:shadow-gray-800/40 sm:flex">
+        <div className="flex flex-col">
+          <div className="w-full flex-wrap overflow-auto sm:flex">
             <div className="px-6 py-4">
-              {pathname.startsWith('/blog') ? (
-                <h3 className="font-bold uppercase text-primary-500">All Posts</h3>
-              ) : (
-                <Link
-                  href={`/blog`}
-                  className="font-bold uppercase text-gray-700 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-500"
-                >
-                  All Posts
-                </Link>
-              )}
-              <ul>
+              <ul className="mt-2 flex flex-row flex-wrap">
+                {pathname.startsWith('/blog') ? (
+                  <li className="my-3">
+                    <h3 className="inline rounded-full bg-gray-100 p-2 px-3 py-2 text-sm font-bold uppercase text-primary-500 shadow-md">
+                      All Posts
+                    </h3>
+                  </li>
+                ) : (
+                  <Link
+                    href={`/blog`}
+                    className="m-1 rounded-full bg-gray-100 p-2 px-3 py-2 text-sm font-medium uppercase text-gray-500 hover:text-primary-500 hover:shadow-md dark:text-gray-300 dark:hover:text-primary-500"
+                  >
+                    All Posts
+                  </Link>
+                )}
                 {sortedTags.map((t) => {
                   return (
                     <li key={t} className="my-3">
                       {pathname.split('/tags/')[1] === slug(t) ? (
-                        <h3 className="inline px-3 py-2 text-sm font-bold uppercase text-primary-500">
+                        <h3 className="inline rounded-full bg-gray-100 p-2 px-3 py-2 text-sm font-bold uppercase text-primary-500 shadow-md">
                           {`${t} (${tagCounts[t]})`}
                         </h3>
                       ) : (
                         <Link
                           href={`/tags/${slug(t)}`}
-                          className="px-3 py-2 text-sm font-medium uppercase text-gray-500 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-500"
+                          className="m-1 rounded-full bg-gray-100 p-2 px-3 py-2 text-sm font-medium uppercase text-gray-500 hover:text-primary-500 hover:shadow-md dark:text-gray-300 dark:hover:text-primary-500"
                           aria-label={`View posts tagged ${t}`}
                         >
                           {`${t} (${tagCounts[t]})`}
@@ -124,7 +128,7 @@ export default function ListLayoutWithTags({
               {displayPosts.map((post) => {
                 const { path, date, title, summary, tags } = post
                 return (
-                  <li key={path} className="py-5">
+                  <li key={path} className="m-2 rounded-[20px] border border-gray-200 p-5">
                     <article className="flex flex-col space-y-2 xl:space-y-0">
                       <dl>
                         <dt className="sr-only">Published on</dt>
